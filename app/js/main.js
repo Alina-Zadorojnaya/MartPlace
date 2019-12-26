89f1a31a01f6665e3a41e0a03e8a89958ad82ca7
Alina-Zadorojna <your_email@whatever.comalinasmartmebel@gmail.com>
2019-12-26 18:54:46 +0200

    'section-slider'

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
 $(function(){
 
+   
+    $('.slider-wrapper').slick({
+        dots: false,
+        infinite: true,
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+
+        prevArrow:'<button class="slick-arrow slick-prev" type="button"><img src="images/icons/chevron-left.png" alt=""></button>',
+        nextArrow:'<button class="slick-arrow slick-next" type="button"><img src="images/icons/chevron-right.png" alt=""></button>'
+
+      });
+            
+
 });
