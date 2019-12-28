eddcc413851fb1a93d130e8d7df65850c3fc21fa
Alina-Zadorojna <your_email@whatever.comalinasmartmebel@gmail.com>
2019-12-28 22:36:16 +0200

    'follower-slider'

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
 $(function(){
 
-   
+ 
     $('.slider-wrapper').slick({
         dots: false,
         infinite: true,
@@ -12,6 +12,19 @@ $(function(){
         nextArrow:'<button class="slick-arrow slick-next" type="button"><img src="images/icons/chevron-right.png" alt=""></button>'
 
       });
-            
+        
+      var mixer = mixitup('.products__item-box');
+
+      $('.followers__slider').slick({
+        dots: false,
+        infinite: true,
+        arrows: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+
+        prevArrow:'<button class="slick-arrow slick-prev" type="button"><img src="images/icons/chevron-left.png" alt=""></button>',
+        nextArrow:'<button class="slick-arrow slick-next" type="button"><img src="images/icons/chevron-right.png" alt=""></button>'
+
+      });
 
 });
