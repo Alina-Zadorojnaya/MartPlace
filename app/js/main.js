4a560b01e0276f003165652184b83e5e8bb23678
Alina-Zadorojna <your_email@whatever.comalinasmartmebel@gmail.com>
2020-02-09 00:35:30 +0200

    'addee'

--- app/js/main.js
+++ app/js/main.js
@@ -19,7 +19,95 @@ $(function(){
         nextArrow:'<button class="slick-arrow slick-next" type="button"><img src="images/icons/chevron-right.png" alt=""></button>'
 
       });
-        
+
+
+
+
+      var $range = $(".js-range-slider"),
+      $inputFrom = $(".js-input-from"),
+      $inputTo = $(".js-input-to"),
+      instance,
+      min=0,
+      max = 400,
+      from = 0,
+      to = 0;
+      
+  
+  $range.ionRangeSlider({
+    skin: "round",
+      type: "double",
+      min: min,
+      max: max,
+      from: 30,
+      to: 300,
+      onStart: updateInputs,
+      onChange: updateInputs
+  });
+  instance = $range.data("ionRangeSlider");
+  
+  function updateInputs (data) {
+    from = data.from;
+      to = data.to;
+      
+      $inputFrom.prop("value", '$ '+ from);
+      $inputTo.prop("value", '$ '+ to);	
+  }
+  
+  $inputFrom.on("input", function () {
+      var val = $(this).prop("value");
+      
+      // validate
+      if (val < min) {
+          val = min;
+      } else if (val > to) {
+          val = to;
+      }
+      
+      instance.update({
+          from: val
+      });
+  });
+  
+  $inputTo.on("input", function () {
+      var val = $(this).prop("value");
+      
+      // validate
+      if (val < from) {
+          val = from;
+      } else if (val > max) {
+          val = max;
+      }
+      
+      instance.update({
+          to: val
+      });
+  });
+
+
+
+
+ 
+    /*  $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 400,
+        from: 30,
+        to: 300,
+        prefix: "$"
+    });*/
+
+
+    $('.icon-list').on('click', function(){
+      $('.probuct__item').addClass('list');
+      $('.icon-list').addClass('active');
+      $('.icon-th-large').removeClass('active')   
+    });
+      
+    $('.icon-th-large').on('click', function(){
+      $('.probuct__item').removeClass('list');
+      $('.icon-th-large').addClass('active');
+      $('.icon-list').removeClass('active');
+    });
       var mixer = mixitup('.products__item-box');
 
       $('.followers__slider').slick({
@@ -34,13 +122,5 @@ $(function(){
 
       });
 
-     /* $('.feedback__slider').slick({
-        infinite: true,
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        
-        prevArrow:'<button class="slick-arrow slick-prev" type="button"><img src="images/icons/left.png" alt=""></button>',
-        nextArrow:'<button class="slick-arrow slick-next" type="button"><img src="images/icons/right.png" alt=""></button>'
-      });*/
-
+ 
 });
